f95852f35b220ecbb5e53f0f2ef0c20c425fdaa0
Samuel Chenot <samuelchenot@gmail.com>
2021-08-12 22:58:01 -0400

    seperation of duties

---
+++ SmiteSavantDataTool/Requests.fs
@@ -0,0 +1,15 @@
+﻿module Requests
+
+open System.Net.Http
+
+let getAsync (client:HttpClient) (url:string) = 
+    async {
+        let! response = client.GetAsync(url) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode () |> ignore
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content
+    }
+
+let createClient = 
+    let httpClient = new HttpClient()
+    httpClient
---
+++ SmiteSavantDataTool/SignatureBuilder.fs
@@ -0,0 +1,28 @@
+﻿module SignatureBuilder
+
+open System
+open System.Security.Cryptography
+open System.Text
+
+type Signature = {devid : string; methodname : string; authkey : string; datetime : string}
+
+let md5 = MD5.Create()
+
+let MD5HashToString (input : string) =
+    input
+    |> Encoding.UTF8.GetBytes
+    |> md5.ComputeHash
+    |> Seq.map (fun c -> c.ToString("X2"))
+    |> Seq.map (fun c -> c.ToLower())
+    |> Seq.reduce (+)
+
+let getDateTime = 
+    DateTime.UtcNow.ToString("yyyyMMddHHmmss")
+
+let buildSignature signature = 
+    MD5HashToString (signature.devid + signature.methodname + signature.authkey + signature.datetime) 
+
+
+
+
+    
